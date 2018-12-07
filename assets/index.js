29d2bc05ff170e3c6ea7f36805e7538f9dc7f7cb
Daniel Kanas <dkanas95@gmail.com>
2018-12-07 14:45:40 +0100

    site finished

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,34 @@
 window.onload = () => {
-  const overlay = document.querySelector('.overlay')
-  const text = document.querySelector('.overlay__text')
-  setTimeout(() => overlay.style.backgroundColor = 'rgba(0, 0, 0, 1)', 300)
-  setTimeout(() => text.style.opacity = 1, 1700)
+  topSectionTextAnimation()
+  initializeListeners()
+  insertEmailAddress()
+}
+
+const topSectionTextAnimation = () => {
+  const header = document.querySelector('.top-section__header')
+  const label = document.querySelector('.top-section__description')
+  const arrowCircle = document.querySelector('.top-section__arrowcircle')
+  setTimeout(() => (header.style.opacity = 1), 500)
+  setTimeout(() => (label.style.opacity = 1), 1000)
+  setTimeout(() => (arrowCircle.style.opacity = 1), 1500)
+}
+
+const initializeListeners = () => {
+  const arrowCircle = document.querySelector('.top-section__arrowcircle')
+  const topSection = document.querySelector('.top-section')
+  arrowCircle.addEventListener('click', () => {
+    const { bottom } = topSection.getBoundingClientRect()
+    window.scrollTo({ top: bottom + window.scrollY, behavior: 'smooth' })
+  })
+}
+
+const insertEmailAddress = () => {
+  const address = ['k', 'a', 'n', 'a', 's', '@', 'm', 'u', 'z', 'y', 'k', '.', 'o', 'r', 'g'].join(
+    ''
+  )
+  const elem = document.querySelector('#email')
+  setTimeout(() => {
+    elem.innerHTML = address
+    elem.href = 'mailto:' + address
+  }, 100)
 }
